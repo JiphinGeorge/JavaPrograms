85d0df8185762790530697b2791731218e7ad787
JiphinGeorge <jiphingeorge80@gmail.com>
2024-09-30 11:52:17 +0530

    ok

--- src/AWTExample.java
+++ src/AWTExample.java
@@ -4,45 +4,45 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class AWTExample extends Applet implements ActionListener {
-    TextField txt = new TextField(20); // Use a number to set the width
+    TextField txt = new TextField(20); 
     String str, actionMessage;
     Button bt_add = new Button("Add");
     Button bt_del = new Button("Delete");
     List l = new List();
 
-    public void init() { // Corrected method name from 'int' to 'init'
+    public void init() { 
         add(bt_add);
         add(bt_del);
         add(txt);
         add(l);
-        bt_add.addActionListener(this); // Corrected syntax
-        bt_del.addActionListener(this); // Corrected syntax
+        bt_add.addActionListener(this); 
+        bt_del.addActionListener(this); 
     }
 
-    public void paint(Graphics g) { // Corrected method name from 'print' to 'paint'
-        g.drawString(actionMessage, 20, 55); // Corrected syntax
+    public void paint(Graphics g) { 
+        g.drawString(actionMessage, 20, 55); 
     }
 
-    public void actionPerformed(ActionEvent event) { // Corrected method name from 'actionperformed' to 'actionPerformed'
+    public void actionPerformed(ActionEvent event) { 
         if (event.getSource() == bt_add) {
             str = txt.getText();
-            if (!str.equals(" ")) { // Corrected condition syntax
+            if (!str.equals(" ")) { 
                 l.add(str);
             }
             txt.requestFocus();
         }
         if (event.getSource() == bt_del) {
             int selectedIndex = l.getSelectedIndex();
-            if (selectedIndex != -1) { // Check if an item is selected
+            if (selectedIndex != -1) { 
                 l.remove(selectedIndex);
             }
         }
         
         String action = event.getActionCommand();
-        if (action.equals("Add")) { // Case-sensitive check for the button text
-            actionMessage = "Add button pressed"; // Fixed variable naming
-        } else if (action.equals("Delete")) { // Fixed syntax
-            actionMessage = "Delete button pressed"; // Fixed variable naming
+        if (action.equals("Add")) { t
+            actionMessage = "Add button pressed"; 
+        } else if (action.equals("Delete")) { 
+            actionMessage = "Delete button pressed"; 
         }
         repaint();
     }
